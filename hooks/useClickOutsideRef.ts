e334f1ea554ae11bcb2bfd9a0598b712a03f92b9
Charlie <lucharles3@gmail.com>
2021-11-19 13:38:13 -0800

    Update useClickOutsideRef.ts

--- hooks/useClickOutsideRef.ts
+++ hooks/useClickOutsideRef.ts
@@ -1,61 +1,83 @@
 // dependencies
-import { useEffect, useRef, MouseEvent, RefObject } from 'react';
+import { useEffect, useRef, MouseEvent, TouchEvent, RefObject } from 'react';
+
+/* TYPES */
+type TapEvent = MouseEvent | TouchEvent;
 
 /**
  * Returns refs that calls an onClick function everytime a click is made outside of that HTML Element
  * Defaults to 1 ref returned - if greater than 1, the refs will be returned in an array
+ * numRefs only takes positive numbers
  */
-const useClickOutsideRef = ( 
-    onClick: ( event: MouseEvent ) => void, 
-    numRefs: number=1
-) => {
-
-    // clickOutside function that will attach the the click event listener to each ref
-    let clickOutsideFn: ( event: MouseEvent ) => void;
-    // either a single ref or a list of refs
-    let refs: RefObject<HTMLElement>[] | RefObject<HTMLElement>;
+const useClickOutsideRef = <T extends HTMLElement>( 
+    onClick: ( event: TapEvent ) => void,
+    numRefs: number=1 // this should only be a positive number
+): RefObject<T>[] | RefObject<T> => {
 
     // creating an array of refs
     if ( numRefs > 1 ) {
-        for ( let i=0; i < numRefs; i++ ) {
-            refs = [];
-            refs.push( useRef( null ) );
-        }
-    } 
-    // creating a single ref
-    else {
-        refs = useRef( null );
-    }
+        const refList: RefObject<T>[] = [];
 
-    if ( Array.isArray( refs ) ) {
-        clickOutsideFn = ( event: MouseEvent ) => {
-            let isFocused: boolean = false;
-            for ( const ref of refs ) {
+        for ( let i=0; i < numRefs; i++ ) {
+            refList.push( useRef( null ) );
+        }
+
+        const clickOutsideFn = ( event: TapEvent ): void => {
+            // if the HTMLElement ref is clicked, isClicked will be false
+            // by default, we assume the HTMLElement ref is clicked (false)
+            let isClicked: boolean = false;
+
+            for ( const ref of refList ) {
                 // when you navigate to a new page, for some reason a nullish ref gets added to the beginning of the refs
                 if ( !ref.current ) continue;
-                if ( !ref.current ) isFocused = true;
-                if ( ref.current.contains( event.target ) ) isFocused = true;
+                if ( !ref.current ) isClicked = true;
+                if ( ref.current.contains( event.target as HTMLElement ) ) isClicked = true;
             }
-            if ( !isFocused ) onClick( event );
+            // the HTMLElement was not clicked - call the onClick function
+            if ( !isClicked ) onClick( event );
         }   
-    } else {
-        clickOutsideFn = ( event: MouseEvent ) => {
-            if ( !refs.current ) return;
-            if ( refs.current.contains( event.target ) ) return;
-            onClick( event );
-        }
-    }
+        // attaching the clickOutsideFn to all the refs in the refList
+        addRefEventListener( clickOutsideFn );
 
+        return refList;
+    } 
+    // creating a single ref
+    else if ( numRefs === 1 ) {
+        const ref: RefObject<T> = useRef( null );
+        
+        const clickOutsideFn = ( event: TapEvent ): void => {
+            if ( !ref.current ) return;
+            if ( ref.current.contains( event.target as HTMLElement ) ) return;
+            // the HTMLElement was not clicked - call the onClick function
+            onClick( event );
+
+        }
+        // attaching the clickOutsideFn to the ref
+        addRefEventListener( clickOutsideFn );
+
+        return ref;
+    }
+    // numRefs specfifed as something other than a positive integer
+    else {
+        throw(`numRefs needs to be a positive number. You specified numRefs as ${numRefs}.`)
+    }
+}
+
+
+const addRefEventListener = ( clickOutsideFn: ( event: TapEvent ) => void ) => {
+    // TO-DO - find a typescript solution for this
     useEffect( () => {
+        // @ts-ignore
         document.addEventListener( 'mousedown', clickOutsideFn );
+        // @ts-ignore
         document.addEventListener( 'touchstart', clickOutsideFn );
         return () => {
+            // @ts-ignore
             document.removeEventListener( 'mousedown', clickOutsideFn );
+            // @ts-ignore
             document.removeEventListener( 'touchstart', clickOutsideFn );
         }
     }, [] );  
-
-    return refs;
 }
 
 export default useClickOutsideRef;
