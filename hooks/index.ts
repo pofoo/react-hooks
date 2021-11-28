14a0cedce1adac5e102559e4df9766d724b40152
Charlie <lucharles3@gmail.com>
2021-11-27 20:04:57 -0800

    focus-trap

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,9 +1,11 @@
 import useClickOutsideRef from './click-outside';
 import { usePointerDown, usePointerUp, usePointerMove } from './events';
+import useFocusTrap from './focus-trap';
 
 export {
     useClickOutsideRef,
     usePointerDown,
     usePointerUp,
     usePointerMove,
+    useFocusTrap,
 }
